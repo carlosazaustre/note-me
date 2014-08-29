f1e08b3180ab465050e5d1d88f56dff25c010726
carlosazaustre <cazaustre@gmail.com>
2014-08-29 14:03:03 +0200

    Modularize routes

--- server.js
+++ server.js
@@ -13,39 +13,8 @@ var db = {} // Memory persitence
 app.use(bodyParser.json('application/json'));
 
 // Routes
-app.post('/notes', function(req, res) {
-  logger.info('POST', req.body);
-
-  var newNote = req.body.note;
-  newNote.id = Date.now();
-
-  db[newNote.id] = newNote;
-
-  res.set('Content-Type', 'application/json');
-  res.status(201);
-
-  res.json({
-    note: db[newNote.id]
-  });
-
-});
-
-app.get('/notes/:id', function(req, res) {
-  logger.info('GET /notes/%s', req.params.id);
-
-  var id = req.params.id;
-  var note = db[id];
-
-  if(!note) {
-    res.status(404);
-    return res.send('Not Found');
-  }
-
-  res.json({
-    note: note
-  });
-
-});
+var notes = require('./lib/notes');
+app.use('/notes', notes);
 
 // Start server if we're not someone else's dependency
 if(!module.parent) {
