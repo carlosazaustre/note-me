e7c164837f39613e188619801c4b2a4ef4c177fa
carlosazaustre <cazaustre@gmail.com>
2014-08-29 15:29:48 +0200

    refactoring routes

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var logger = require('./lib/logger');
 
-
 // Locals
 var app = module.exports = express();
 var port = process.env.PORT | 3000;
-var db = {} // Memory persitence
 
 // Middlewares
 app.use(bodyParser.json('application/json'));
 
 // Routes
 var notes = require('./lib/notes');
-app.use('/notes', notes);
+app.use(notes);
 
 // Start server if we're not someone else's dependency
 if(!module.parent) {
