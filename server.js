1381d562f1261f76dc191e7471be9588e4742ffd
carlosazaustre <cazaustre@gmail.com>
2014-08-29 13:57:09 +0200

    added test for get and route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var logger = require('./lib/logger');
 // Locals
 var app = module.exports = express();
 var port = process.env.PORT | 3000;
+var db = {} // Memory persitence
 
 // Middlewares
 app.use(bodyParser.json('application/json'));
@@ -16,13 +17,32 @@ app.post('/notes', function(req, res) {
   logger.info('POST', req.body);
 
   var newNote = req.body.note;
-  newNote.id = '123';
+  newNote.id = Date.now();
+
+  db[newNote.id] = newNote;
 
   res.set('Content-Type', 'application/json');
   res.status(201);
 
   res.json({
-    note: newNote
+    note: db[newNote.id]
+  });
+
+});
+
+app.get('/notes/:id', function(req, res) {
+  logger.info('GET /notes/%s', req.params.id);
+
+  var id = req.params.id;
+  var note = db[id];
+
+  if(!note) {
+    res.status(404);
+    return res.send('Not Found');
+  }
+
+  res.json({
+    note: note
   });
 
 });
--- test/notes.js
+++ test/notes.js
@@ -25,7 +25,7 @@ describe('Notes collection [/notes]', function() {
           .expect(201)
           .expect('Content-Type', /application\/json/)
           .end(function(err, res) {
-            var notes;
+            var note;
 
             var body = res.body;
             console.log('body', body);
@@ -43,6 +43,56 @@ describe('Notes collection [/notes]', function() {
 
             done(err);
           });
+    });
+  });
+
+  describe('GET', function() {
+    it('should get a note with an id', function(done) {
+      var data = {
+        "note" : {
+          "title": "A new note",
+          "description": "Note description",
+          "type": "text",
+          "body": "This is the body"
+        }
+      };
+
+      request
+        .post('/notes')
+        .set('Accept', 'application/json')
+        .send(data)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+        .end(function(err, res) {
+          var body = res.body;
+          console.log('body', body);
+          var id = body.note.id;
+
+          request
+            .get('/notes/' + id)
+            .set('Accept', 'application/json')
+            .send()
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+            .end(function(err, res) {
+              var note;
+              var body = res.body;
+              console.log('GET body', body);
+
+              // Note exists
+              expect(body).to.have.property('note');
+              note = body.note;
+
+              // Properties
+              expect(note).to.have.property('id', id);
+              expect(note).to.have.property('title', 'A new note');
+              expect(note).to.have.property('description', 'Note description');
+              expect(note).to.have.property('type', 'text');
+              expect(note).to.have.property('body', 'This is the body');
+
+              done(err);
+            });
+        });
     });
   });
 
