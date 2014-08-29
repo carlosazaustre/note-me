4fd6b70835e91efc0b7cb83577626d40dc5601e2
carlosazaustre <cazaustre@gmail.com>
2014-08-29 11:47:47 +0200

    added jshint and first test write

--- test/notes.js
+++ test/notes.js
@@ -8,7 +8,40 @@ describe('Notes collection [/notes]', function() {
 
   describe('POST', function() {
     it('should create a note', function(done) {
-        return true;
+        var data = {
+          "note" : {
+            "title": "A new note",
+            "description": "Note description",
+            "type": "text",
+            "body": "This is the body"
+          }
+        };
+
+        request
+          .post('/notes')
+          .set('Accept', 'application/json')
+          .send(data)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+          .end(function(err, res) {
+            var note;
+
+            var body = res.body;
+            console.log(body);
+
+            // Note exists
+            expect(body).to.have.property('note');
+            note = body.note;
+
+            // Properties
+            expect(note).to.have.property('title', 'A new note');
+            expect(note).to.have.property('description', 'Note description');
+            expect(note).to.have.property('type', 'text');
+            expect(note).to.have.property('body', 'This is the body');
+            expect(note).to.have.property('id');
+
+            done(err);
+          });
     });
   });
 
